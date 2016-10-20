12809b5a522aeea022946c75df786e9c6e4a7dbc
bin0m <levitg@gmail.com>
2016-10-20 17:41:16 +0300

    Project Create, added timer

---
+++ GregoryETPStatisticsParser/ServiceGregory.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using System.IO;
+
+namespace GregoryETPStatisticsParser
+{
+    public partial class ServiceGregory : ServiceBase
+    {
+        public enum ServiceState
+        {
+            SERVICE_STOPPED = 0x00000001,
+            SERVICE_START_PENDING = 0x00000002,
+            SERVICE_STOP_PENDING = 0x00000003,
+            SERVICE_RUNNING = 0x00000004,
+            SERVICE_CONTINUE_PENDING = 0x00000005,
+            SERVICE_PAUSE_PENDING = 0x00000006,
+            SERVICE_PAUSED = 0x00000007,
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct ServiceStatus
+        {
+            public long dwServiceType;
+            public ServiceState dwCurrentState;
+            public long dwControlsAccepted;
+            public long dwWin32ExitCode;
+            public long dwServiceSpecificExitCode;
+            public long dwCheckPoint;
+            public long dwWaitHint;
+        };
+
+        System.Timers.Timer MainActionTimer;
+        DateTime ScheduleTimeToStart;
+        const int mainIntervalInSeconds = 30 ;
+        const string pathLogFile = @"ParserLog.txt";
+
+        public ServiceGregory()
+        {
+            InitializeComponent();
+            MainActionTimer = new System.Timers.Timer();
+            ScheduleTimeToStart = DateTime.Today.AddSeconds(10); 
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            // Update the service state to Start Pending.
+            ServiceStatus serviceStatus = new ServiceStatus();
+            serviceStatus.dwCurrentState = ServiceState.SERVICE_START_PENDING;
+            serviceStatus.dwWaitHint = 100000;
+            SetServiceStatus(this.ServiceHandle, ref serviceStatus);
+
+            File.AppendAllText(pathLogFile, "Service started " + DateTime.Now.ToString("dd/MM/yyyy hh:mm:ss"));
+
+            // For first time, set amount of seconds between current time and schedule time
+            MainActionTimer.Enabled = true;
+            MainActionTimer.Interval = ScheduleTimeToStart.Subtract(DateTime.Now).TotalSeconds * 1000;
+            MainActionTimer.Elapsed += new System.Timers.ElapsedEventHandler(Timer_Elapsed);
+
+            // Update the service state to Running.
+            serviceStatus.dwCurrentState = ServiceState.SERVICE_RUNNING;
+            SetServiceStatus(this.ServiceHandle, ref serviceStatus);
+        }
+
+        protected void Timer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            File.AppendAllText(pathLogFile, "Service timer action started " + DateTime.Now.ToString("dd/MM/yyyy hh:mm:ss"));
+
+            // 1. Process Schedule Task
+            // ----------------------------------
+            // Add code to Process your task here
+            // ----------------------------------
+
+
+            // 2. If tick for the first time, reset next run to Const Interval
+            if (MainActionTimer.Interval != mainIntervalInSeconds * 1000)
+            {
+                MainActionTimer.Interval = mainIntervalInSeconds * 1000;
+            }
+        }
+
+        [DllImport("advapi32.dll", SetLastError = true)]
+        private static extern bool SetServiceStatus(IntPtr handle, ref ServiceStatus serviceStatus);
+
+
+        protected override void OnStop()
+        {
+            // Update the service state to Stop
+            ServiceStatus serviceStatus = new ServiceStatus();
+            serviceStatus.dwCurrentState = ServiceState.SERVICE_STOPPED;
+            SetServiceStatus(this.ServiceHandle, ref serviceStatus);
+
+            File.AppendAllText(pathLogFile, "Service stopped " + DateTime.Now.ToString("dd/MM/yyyy hh:mm:ss"));
+        }
+    }
+}

889dde4470cf8a679ee92b1c61a5f12a3beb97f9
bin0m <levitg@gmail.com>
2016-11-03 19:14:25 +0300

    Query sql updated (all logic inside query) csv output fixed,but still there are problems

--- GregoryETPStatisticsParser/request.sql
+++ GregoryETPStatisticsParser/request.sql
@@ -1,8 +1,9 @@
-SELECT TOP 100 p.PurchaseNumber, FullTitle, OrganizerID, party.ContactName, AuctionStartDate, p.BargainTypeID, bt.Name, bd.inn, arbitrageManagerID, arbitrageTribunalNumber, p.PurchaseStatusID, ps.PurchaseStatusDesc
+SELECT p.PurchaseNumber, FullTitle, OrganizerID, party.ContactName, AuctionStartDate, p.BargainTypeID, bt.Name, bd.inn, LEFT(bd.inn,2) AS Region,kladr.NAME,  arbitrageManagerID, (am.LastName+ ' '+ am.FirstName+' '+ am.MiddleName) AS arbitrageManagerName , arbitrageTribunalNumber, p.PurchaseStatusID, ps.PurchaseStatusDesc
 from Purchase p 
 Inner join bankruptDetails bd  on p.purchaseID = bd.purchaseID
---join Party party2 on bd.ArbitrageManagerID = party2.PartyID  
 Inner join Party party on p.OrganizerID = party.PartyID  
+Inner join ArbitrageManager am on bd.ArbitrageManagerID = am.ManagerID
 Inner join BargainType bt on p.BargainTypeID = bt.BargainTypeID  
 Inner join PurchaseStatus ps on p.PurchaseStatusID = ps.PurchaseStatusID
+Inner join KLADR kladr on kladr.CODE = (LEFT(bd.inn,2)+'00000000000')  
 where p.PurchaseStatusID in (5,7,14)
